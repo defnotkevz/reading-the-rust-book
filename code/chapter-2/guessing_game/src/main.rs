06c95c1172481b7948e04f0943775e326ecdd0fc
Kevin Roy <kevnroy1@gmail.com>
2025-05-10 17:35:42 +0530

    starting chapter2

---
+++ code/chapter-2/guessing_game/src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+//yo, i'm a comment.
+/* I am also
+ * a better comment */
+
+fn main() {
+    println!("Guess the number");
+
+    println!("Please input your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+    println!("You guessed: {}", guess);
+}
